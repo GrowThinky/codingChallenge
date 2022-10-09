4f9cee76a8af8a1a7d44939e8eb6d9b8a4e67c0f
Christoph Heindl <christoph@Christophs-MacBook-Pro-2.local>
2022-10-09 14:59:42 -0400

    implemented separate methods for all input types

--- heindl.codingChallenge/src/heindl/codingChallenge/Roman.java
+++ heindl.codingChallenge/src/heindl/codingChallenge/Roman.java
@@ -6,28 +6,68 @@ public class Roman {
 
 	String input;
 	String[] tokens;
-	HashMap<String, Integer> dictionary;
+	HashMap<String, Integer> newDictionary;
+	HashMap<String, Integer> romanDictionary;
 
 	public Roman() {
-		dictionary = new HashMap<String, Integer>();
-		dictionary.put("X", 10);
-		dictionary.put("V", 5);
-		dictionary.put("I", 1);
+		romanDictionary = new HashMap<String, Integer>();
+		romanDictionary.put("X", 10);
+		romanDictionary.put("V", 5);
+		romanDictionary.put("I", 1);
 	}
 
+	
+	/*
+	 * Translates a given String input to integer. 
+	 */
 	public int translate(String input) {
 		tokens = input.split(" ");
 		return eval(0, tokens);
 	}
 
-
-	
-	public int eval(int i, String[] tokens) {
-		if (i == tokens.length - 1) {
-			return dictionary.get(tokens[i]);
+	/*
+	 * Fills the dictionary with new key value pairs. 
+	 */
+	private void learn(String input) {
+		tokens = input.split(" is ");
+		if (tokens.length != 2) {
+			throw new IllegalArgumentException();
 		}
-		int first = dictionary.get(tokens[i]);
-		int second = dictionary.get(tokens[i + 1]);
+		newDictionary.put(tokens[0], romanDictionary.get(tokens[1]));
+	}
+
+	/*
+	 * Infers value of unknown symbol and adds it as new key-value pair to dictionary 
+	 */
+	private void inferValue(String input) {
+		String[] parts = input.split(" is ");
+		int result = Integer.valueOf(parts[1].split(" ")[0]);
+		tokens = parts[0].split(" ");
+		String[] knownSymbols = new String[tokens.length-1];
+		String newSymbol = null;
+		int j = 0;
+		for (int i = 0; i < tokens.length; i++) {
+			if (newDictionary.containsKey(knownSymbols[i])) {
+				knownSymbols[j] = tokens[i];
+				j++;
+			} else {
+				newSymbol = tokens[i];
+			}
+		}
+		int newTranslation = Math.abs(result - eval(0, knownSymbols));
+		newDictionary.put(newSymbol, newTranslation);
+	}
+
+	/*
+	 * Evaluates an array of known symbols to an integer value.
+	 */
+	private int eval(int i, String[] tokens) {
+		if (i == tokens.length - 1) {
+			return newDictionary.get(tokens[i]);
+		}
+		
+		int first = newDictionary.get(tokens[i]);
+		int second = newDictionary.get(tokens[i + 1]);
 
 		if (first >= second) {
 			return first + eval(i + 1, tokens);
@@ -36,5 +76,6 @@ public class Roman {
 		}
 
 	}
+	
 
 }
