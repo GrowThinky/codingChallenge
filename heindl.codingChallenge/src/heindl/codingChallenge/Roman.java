c126c655649146d3cd5b1ad8f88ba1f66c2efdb5
Christoph Heindl <christoph@Christophs-MacBook-Pro-2.local>
2022-10-09 14:30:05 -0400

    implemented simple evaluation strategy

--- heindl.codingChallenge/src/heindl/codingChallenge/Roman.java
+++ heindl.codingChallenge/src/heindl/codingChallenge/Roman.java
@@ -3,28 +3,38 @@ package heindl.codingChallenge;
 import java.util.HashMap;
 
 public class Roman {
-	
-	String input; 
+
+	String input;
 	String[] tokens;
-	HashMap<String,Integer> dictionary;
-	
-	public  Roman() {
-		dictionary = new HashMap<String,Integer>();
+	HashMap<String, Integer> dictionary;
+
+	public Roman() {
+		dictionary = new HashMap<String, Integer>();
 		dictionary.put("X", 10);
 		dictionary.put("V", 5);
 		dictionary.put("I", 1);
 	}
-	
-	public int translate(String input)  {
+
+	public int translate(String input) {
 		tokens = input.split(" ");
-		return eval(0);
+		return eval(0, tokens);
 	}
+
+
 	
-	
-	public int eval(int i) {
-		
-		return 0;
-		
+	public int eval(int i, String[] tokens) {
+		if (i == tokens.length - 1) {
+			return dictionary.get(tokens[i]);
+		}
+		int first = dictionary.get(tokens[i]);
+		int second = dictionary.get(tokens[i + 1]);
+
+		if (first >= second) {
+			return first + eval(i + 1, tokens);
+		} else {
+			return (-1) * first + eval(i + 1, tokens);
+		}
+
 	}
 
 }
